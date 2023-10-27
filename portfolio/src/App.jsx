ff928afac563b3a50d64e76fb928acbfaf7c4c24
samt11345 <Samuel.tahjai.williams@gmail.com>
2023-10-26 20:22:33 -0400

    building portfolio

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Sidenav from './Components/Sidenav'
+
+function App() {
+
+  return (
+    <>
+    <Sidenav />
+    </>
+  )
+}
+
+export default App
