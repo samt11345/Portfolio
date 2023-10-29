71e9bc07d270b8bc0dbeefb3251e2f542ae5cdf3
samt11345 <Samuel.tahjai.williams@gmail.com>
2023-10-29 12:41:58 -0400

    adding pictures and personal info

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Sidenav from './Components/Sidenav'
 import Main from './Components/Main'
 import Work from './Components/Work'
+import projects from './Components/Projects'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <Sidenav />
     <Main />
     <Work />
+    <projects />
     </div>
     
   )
